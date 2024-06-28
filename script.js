d51d2e25f739e9f7edc26136914bfbde803f3956
Adekoya Emmanuel <97315230+YourRealPops@users.noreply.github.com>
2024-06-28 01:22:32 +0100

    worked on the header and added new pagaes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+
 const asideSection = document.querySelector("aside");
 const mainSection = document.querySelector("main");
 
